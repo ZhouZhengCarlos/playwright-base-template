51d4734e8a02783c4578229ca1d4acde8113adb2
Carlos Zhou Zheng <carloszhou0@gmail.com>
2024-12-10 12:39:53 -0600

    remove

--- src/fixtures/helpers.ts
+++ src/fixtures/helpers.ts
@@ -1,11 +1,7 @@
 import { test as base } from '@playwright/test';
-import * as randomHelper from  '@helpers/randomHelper';
 import * as reporterHelper from '@helpers/reporterAnnotationHelper';
 
 export const test = base.extend<{ randomHelper: typeof randomHelper, reporterhelper: typeof reporterHelper}>({
-    randomHelper: async ({}, use) => {
-        await use(randomHelper);
-    },
     reporterhelper: async ({}, use) => {
         await use(reporterHelper);
     },
